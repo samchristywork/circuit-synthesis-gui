71af929307d5063b358fa6ee8f856f4daee3f475
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-29 21:43:26 -0500

    feat: Add keybinds for c and n
    `c` creates a new component, and `n` adds a subassembly to the current
    component.

--- event.js
+++ event.js
@@ -18,6 +18,27 @@ document.addEventListener("keydown", function(e) {
     } else {
       selectedSubassembly--;
     }
+  } else if (e.keyCode == 67) { // c
+    var type = prompt("type", "");
+
+    if (type != null) {
+      components.push({
+        type: type,
+        subassembly: [],
+        links: [],
+      });
+      currentComponent = type;
+    }
+  } else if (e.keyCode == 78) { // n
+    var name = prompt("name", "");
+    if (name != null) {
+      var type = prompt("type", "");
+      if (type != null) {
+        var i = findComponent(currentComponent);
+        components[i].subassembly.push([name, type, 0, 0]);
+        selectedSubassembly = components[i].subassembly.length - 1;
+      }
+    }
   } else if (e.keyCode >= 49 && e.keyCode <= 57) { // 1-9
     selectedSubassembly = e.keyCode - 49;
   } else {
