cb445952b793a1be8dad505d799e19889c68dd6e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-01 22:22:09 -0500

    feat: Add IO primitives
    These are useful for defining inputs and outputs to our assemblies. This
    should be all the IO that is needed, however a bus IO might be useful
    too.

--- component.js
+++ component.js
@@ -101,4 +101,38 @@ var components = [
       }
     ]
   },
+  {
+    "type": "input",
+    "x": 0,
+    "y": 200,
+    "id": "b",
+    "subassembly": [],
+    "links": [],
+    "physical": [
+      {
+        "x": 0,
+        "y": 0,
+        "blocks": [
+          "A",
+        ]
+      }
+    ]
+  },
+  {
+    "type": "output",
+    "x": 400,
+    "y": 100,
+    "id": "o",
+    "subassembly": [],
+    "links": [],
+    "physical": [
+      {
+        "x": 0,
+        "y": 0,
+        "blocks": [
+          "a",
+        ]
+      }
+    ]
+  },
 ]
