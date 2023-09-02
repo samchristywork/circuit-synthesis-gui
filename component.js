02f603fb49103147b6dfb2982fcab68349cdc7bb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-01 22:20:59 -0500

    feat: Add gate primitives
    These are the basic gates that will be used in many assemblies. There
    are some primitives that are missing that I will add later.

---
+++ component.js
@@ -0,0 +1,104 @@
+var components = [
+  {
+    "type": "or",
+    "subassembly": [
+      [
+        "a",
+        "input",
+        0,
+        0
+      ],
+      [
+        "b",
+        "input",
+        0,
+        200
+      ],
+      [
+        "o",
+        "output",
+        400,
+        100
+      ]
+    ],
+    "links": [],
+    "physical": [
+      {
+        "x": 0,
+        "y": 0,
+        "blocks": [
+          "a|A",
+          " b"
+        ]
+      }
+    ]
+  },
+  {
+    "type": "xor",
+    "subassembly": [
+      [
+        "a",
+        "input",
+        0,
+        0
+      ],
+      [
+        "b",
+        "input",
+        0,
+        200
+      ],
+      [
+        "o",
+        "output",
+        400,
+        100
+      ]
+    ],
+    "links": [],
+    "physical": [
+      {
+        "x": 0,
+        "y": 0,
+        "blocks": [
+          "a^A",
+          " b"
+        ]
+      }
+    ]
+  },
+  {
+    "type": "and",
+    "subassembly": [
+      [
+        "a",
+        "input",
+        0,
+        0
+      ],
+      [
+        "b",
+        "input",
+        0,
+        200
+      ],
+      [
+        "o",
+        "output",
+        400,
+        100
+      ]
+    ],
+    "links": [],
+    "physical": [
+      {
+        "x": 0,
+        "y": 0,
+        "blocks": [
+          "a&A",
+          " b"
+        ]
+      }
+    ]
+  },
+]
